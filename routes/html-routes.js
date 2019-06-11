6fe49ddba9ed394d46d65cbb1cfb5c5b9593715a
Brad <bkhoffman@gmail.com>
2019-06-10 22:48:12 -0500

    added some new images, updated styling for login and signup

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -13,7 +13,7 @@ module.exports = function(app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  app.get("/", function(req, res) {
+  app.get("", function(req, res) {
     // res.sendFile(path.join(__dirname, "../public/index.html"));
     res.render("index");
   });

b46d154fb3fabb19f26d6b1cdd7ba32200b2872b
Brad <bkhoffman@gmail.com>
2019-06-10 21:05:04 -0500

    added recBuild/Reviews handlbars, changed html routes to render handlebars pages

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -14,26 +14,39 @@ module.exports = function(app) {
 
   // index route loads view.html
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    // res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.render("index");
   });
-
   // cms route loads cms.html
-  app.get("/results", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/results.html"));
-  });
-
-  // blog route loads blog.html
+  // app.get("/results", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/results.html"));
+  //   // res.render("results");
+  // });
+  // survey route loads survey handlebars
   app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
+    // res.sendFile(path.join(__dirname, "../public/survey.html"));
+    res.render("survey");
   });
-
+  // recBuild route loads recommendations handlebars
   app.get("/recBuild", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/recBuild.html"));
+    // res.sendFile(path.join(__dirname, "../public/recBuild.html"));
+    res.render("recBuild");
   });
+  // reviews route loads reviews handlebars
   app.get("/reviews", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/reviews.html"));
+    // res.sendFile(path.join(__dirname, "../public/reviews.html"));
+    res.render("reviews");
   });
-
+  // signup route loads signup handlebars
+  app.get("/signup", function(req, res) {
+    // res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.render("signup");
+  });
+  // survey login loads login handlebars
+  app.get("/login", function(req, res) {
+    // res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.render("login");
+  });
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
